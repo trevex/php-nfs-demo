bd1ff50015c75f13f9c6cf211a5dbaa531aaad46
Niklas Voss <niklas.voss@gmail.com>
2022-06-21 14:02:57 +0200

    Add basic nix environment

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "cloud-run-php-nfs-demo";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    let
+      overlays = [ ];
+    in
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system overlays; };
+      in
+      rec {
+        devShell = pkgs.mkShell rec {
+          name = "cloud-run-php-nfs-demo";
+
+          buildInputs = with pkgs; [
+            terraform
+            tfsec
+            google-cloud-sdk
+            kubectl
+            kubernetes-helm
+            go
+          ];
+        };
+      }
+    );
+}
